0f7bcef52ee1812d4a4884d8503d0fbe1f11f311
Esa Krissa <esakrissa.wayan@gmail.com>
2025-03-06 09:25:23 +0800

    Fix deployment process to handle port conflicts and PM2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,17 @@ module.exports = {
     name: "travel-agency-app",
     script: "index.js",
     instances: 1,
+    exec_mode: "fork",
     autorestart: true,
     watch: false,
     max_memory_restart: "200M",
     env: {
       NODE_ENV: "development",
+      PORT: 8080
     },
     env_production: {
       NODE_ENV: "production",
+      PORT: 8080
     },
     log_date_format: "YYYY-MM-DD HH:mm:ss",
     out_file: "app.log",
